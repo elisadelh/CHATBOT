027fe273c5ba569af602a7920094e1c9289acb4d
ElisaDelhomme <elisa.delhomme@ensea.fr>
2022-01-12 08:50:00 +0100

    [DEV] Création du projet sous CubeAI

---
+++ test-cubeai-from-cubemx/Debug/X-CUBE-AI/App/chatbot_data.d
@@ -0,0 +1,6 @@
+X-CUBE-AI/App/chatbot_data.o: ../X-CUBE-AI/App/chatbot_data.c \
+ ../X-CUBE-AI/App/chatbot_data.h ../Middlewares/ST/AI/Inc/ai_platform.h
+
+../X-CUBE-AI/App/chatbot_data.h:
+
+../Middlewares/ST/AI/Inc/ai_platform.h:
